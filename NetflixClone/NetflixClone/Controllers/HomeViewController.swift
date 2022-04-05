61ad4cd7d8639d2fcffc1fbd81bb65dc78d697bb
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-04-05 04:59:53 +0200

    Customize Table View Sections & Set navigationBar Hidden when scrolling Down

--- NetflixClone/NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/NetflixClone/Controllers/HomeViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     //MARK:- Vars
+    
+    let sectionTitles : [String] = ["Trending Movies", "Popular" ,"Trending TV" , "Upcomming Movies", "Top Rated"]
+    
     private let tableView : UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -20,7 +23,7 @@ class HomeViewController: UIViewController {
     //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         view.addSubview(tableView)
         
@@ -31,6 +34,8 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height / 2))
         tableView.tableHeaderView = headerView
         
+        configureNavBar()
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -39,15 +44,39 @@ class HomeViewController: UIViewController {
         
     }
     
+    
+    //MARK:- Configure Navigation Controller Function
+    func configureNavBar()  {
+       
+       
+        let containerView = UIControl(frame: CGRect.init(x: 0, y: 0, width: 30, height: 30))
+         //  containerView.addTarget(self, action: #selector(handleSearch), for: .touchUpInside)
+           let image = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 30, height: 30))
+           image.image = UIImage(named: "netflixLogo")
+           containerView.addSubview(image)
+           let netflixLogo = UIBarButtonItem(customView: containerView)
+           netflixLogo.width = 20
+           navigationItem.leftBarButtonItem = netflixLogo
+    
 
-
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+    
+        
+        navigationController?.navigationBar.tintColor = .systemPink 
+    }
+    
+    
+    
 }
 
 //MARK:- HomeViewController Extension for TableView
 extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
-  
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        20
+        sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -71,5 +100,32 @@ extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
         return 40
     }
     
+    //set the title for sectioins
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    // this function to setup style for section textlabel
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else  {return}
+        
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.textColor = .white
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+        
+    }
+    
+    // this function to hide navigationBar when scrolling down
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+     
+        let defualtOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defualtOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX : 0 ,y : min(0, -offset))
+        
+    }
+    
     
 }
+
