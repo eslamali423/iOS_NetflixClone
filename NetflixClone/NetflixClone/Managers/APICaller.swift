dfbabb3d3c7d2bf0dac2291534c8c29dca8a15c7
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-04-06 01:03:04 +0200

    Fetch DiscoverdMovies& SearchMovie by Query & Get YoutubeVideoResponse Form API

--- NetflixClone/NetflixClone/Managers/APICaller.swift
+++ NetflixClone/NetflixClone/Managers/APICaller.swift
@@ -14,10 +14,10 @@ class APICaller {
     struct Constants {
        static let API_KEY = "78bca5a77288aadbe46acb53c5cf6e5b"
         static let baseUrl = "https://api.themoviedb.org"
-       // https://api.themoviedb.org/3/trending/all/day?api_key=78bca5a77288aadbe46acb53c5cf6e5b
-        //https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1
-        //https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
-        //https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1
+        static let youtube_API_KEY = "AIzaSyCGusUZKfChyH0RGyiIZ6SsqxHmcHRGUfE"
+       static let youtube_BaseUrl = "https://youtube.googleapis.com/youtube/v3/search?"
+        //https://youtube.googleapis.com/youtube/v3/search?key=[YOUR_API_KEY]
+
     }
 
     enum APIError :Error  {
@@ -138,7 +138,85 @@ class APICaller {
         task.resume()
         
     }
-
     
+    //MARK:- Discovered Movies
+    func getDiscoverdMovies(completion : @escaping (Result<[Movie], Error>)->Void)  {
+    
+        guard let url = URL(string:         "\(Constants.baseUrl)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {
+            return
+        }
+        
+        let task =  URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data , error == nil else  {return}
+            
+            do {
+                let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                completion(.success(result.results))
+            }catch {
+                print(error.localizedDescription)
+                completion(.failure(APIError.faildToGetData))            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    //MARK:- Search With query
+    func searchMovies(query : String,completion : @escaping (Result<[Movie], Error>)->Void)  {
+        guard let filteredQuery = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            return
+        }
+   
+        guard let url = URL(string:         "\(Constants.baseUrl)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(filteredQuery)") else {
+            return
+        }
+        
+        let task =  URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data , error == nil else  {return}
+            
+            do {
+                let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                completion(.success(result.results))
+            }catch {
+                print(error.localizedDescription)
+                completion(.failure(APIError.faildToGetData))            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    
+    //MARK:- Search With query
+    func getYoutubeMovie(query : String, completion : @escaping (Result<VideoElement, Error>)->Void)  {
+        
+        guard let filteredQuery = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            return
+        }
+   
+        
+        
+        guard let url = URL(string:  "\(Constants.youtube_BaseUrl)q=\(filteredQuery)&key=\(Constants.youtube_API_KEY)") else {
+     
+            return
+        }
+     
+        
+        let task =  URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data , error == nil else  {
+                
+                return}
+            
+            do {
+                let result = try JSONDecoder().decode(YoutubeResponse.self, from: data)
+                print(result)
+            }catch {
+                print("EEEERRRRORRROROORORO")
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+        
+    }
 
 }
