f1c94a98d5b15babb3c6f3d7e4d2454012f0c172
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-04-06 01:00:49 +0200

    Create SearchResultViewController and get the data from SearchViewModel,

---
+++ NetflixClone/NetflixClone/Controllers/SearchResultViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SearchResultViewController.swift
+//  NetflixClone
+//
+//  Created by Eslam Ali  on 05/04/2022.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    //MARK:- Vars
+    
+    public  let collectionView : UICollectionView = {
+         let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: (UIScreen.main.bounds.width / 3) - 10, height: 200)
+        layout.minimumInteritemSpacing = 0
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: MovieCollectionViewCell.identifier)
+        
+        return collectionView
+    }()
+    
+    public var  movies : [Movie] = []
+    
+    //MARK:- Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+        
+    }
+
+
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: MovieCollectionViewCell.identifier, for: indexPath) as? MovieCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: movies[indexPath.row].poster_path ?? "")
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+}
--- NetflixClone/NetflixClone/Controllers/SearchViewController.swift
+++ NetflixClone/NetflixClone/Controllers/SearchViewController.swift
@@ -9,21 +9,112 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    //MARK:- Vars
+    
+    private var tableView : UITableView = {
+        let table =  UITableView()
+        table.register(MovieTableViewCell.self, forCellReuseIdentifier: MovieTableViewCell.identifier)
+        return table
+    }()
+    
+    var viewModel = SearchViewModel()
+    
+    private let searchController : UISearchController = {
+       let controller = UISearchController(searchResultsController: SearchResultViewController())
+        controller.searchBar.placeholder = "Search for a Movie or a Tv Show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+
+        
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        navigationController?.navigationBar.tintColor = .systemPink 
+        navigationItem.searchController = searchController
+        
+        fetchData()
+        searchController.searchResultsUpdater = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
+    
+//MARK:- Get Discoverd Movies Data
+    func fetchData(){
+        viewModel.getDiscoverdMovies { (isSuccess) in
+            if isSuccess{
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+    }
+ 
 
 }
+
+
+extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.discoverdMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifier, for: indexPath) as? MovieTableViewCell else  {
+            print("dont get the cell")
+            return UITableViewCell()
+        }
+        let data = viewModel.discoverdMovies[indexPath.row]
+        cell.configure(model: data)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+        
+    }
+  
+}
+
+extension SearchViewController : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultController = searchController.searchResultsController as? SearchResultViewController
+              else {
+            return
+        }
+        
+        viewModel.search(query: query) { (isSuccess) in
+            if isSuccess {
+                DispatchQueue.main.async {
+                    resultController.movies = self.viewModel.searchedMovies
+                    resultController.collectionView.reloadData()
+                }
+            }
+        }
+    }
+    
+    
+}
+
+
+
